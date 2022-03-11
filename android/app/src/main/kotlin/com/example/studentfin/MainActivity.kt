67b928663676f1327aa22d2224eff7ab3d3b8af6
sidhagarwal05 <sidhagarwal05@gmail.com>
2022-03-11 11:50:37 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/studentfin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.studentfin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/login_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
+import 'package:studentfin/welcome_screen.dart';
+import 'package:auth_buttons/auth_buttons.dart';
+import 'auth.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  LoginPageState createState() => LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(37, 150, 190, 1),
+      // rgb(37, 150, 190)
+      body: Container(
+        color: Colors.indigoAccent,
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Image(
+                image: AssetImage("assets/logo.png"),
+                height: 300,
+                width: 300,
+              ),
+              const SizedBox(height: 25),
+              GoogleAuthButton(
+                onPressed: () {
+                  signInWithGoogle().whenComplete(() {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return WelcomeScreen();
+                        },
+                      ),
+                    );
+                  });
+                },
+                darkMode: false,
+                style: AuthButtonStyle(
+                  iconType: AuthIconType.outlined,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
